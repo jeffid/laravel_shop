54b870ac756e67d7afcf4db23cbfd54055ec4bbc
jeff <waagik@gmail.com>
2020-02-13 23:53:03 +0800

    1、前台商品列表支持按类目筛选 2、根据当前商品类通过模型获取器得到其父类，并加入到面包屑中

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,6 +6,7 @@ use App\Exceptions\InvalidRequestException;
 use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class ProductsController extends Controller
 {
@@ -16,7 +17,7 @@ class ProductsController extends Controller
         // 判断是否有提交 search 参数，如果有就赋值给 $search 变量
         // search 参数用来模糊搜索商品
         if ($search = $request->input('search', '')) {
-            $like = '%'.$search.'%';
+            $like = '%' . $search . '%';
             // 模糊搜索商品标题、商品详情、SKU 标题、SKU描述
             $builder->where(function ($query) use ($like) {
                 $query->where('title', 'like', $like)
@@ -27,7 +28,22 @@ class ProductsController extends Controller
                     });
             });
         }
-
+        
+        // 如果有传入 category_id 字段，并且在数据库中有对应的类目
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            // 如果这是一个父类目
+            if ($category->is_directory) {
+                // 则筛选出该父类目下所有子类目的商品
+                $builder->whereHas('category', function ($query) use ($category) {
+                    // 这里的逻辑参考本章第一节
+                    $query->where('path', 'like', $category->path . $category->id . '-%');
+                });
+            } else {
+                // 如果这不是一个父类目，则直接筛选此类目下的商品
+                $builder->where('category_id', $category->id);
+            }
+        }
+        
         // 是否有提交 order 参数，如果有就赋值给 $order 变量
         // order 参数用来控制商品的排序规则
         if ($order = $request->input('order', '')) {
@@ -40,40 +56,42 @@ class ProductsController extends Controller
                 }
             }
         }
-
+        
         $products = $builder->paginate(16);
-
+        
         return view('products.index', [
             'products' => $products,
-            'filters'  => [
+            'filters' => [
                 'search' => $search,
-                'order'  => $order,
+                'order' => $order,
             ],
+            // 等价于 isset($category) ? $category : null
+            'category' => $category ?? null,
         ]);
     }
-
+    
     public function show(Product $product, Request $request)
     {
         if (!$product->on_sale) {
             throw new InvalidRequestException('商品未上架');
         }
-
+        
         $favored = false;
         // 用户未登录时返回的是 null，已登录时返回的是对应的用户对象
-        if($user = $request->user()) {
+        if ($user = $request->user()) {
             // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
             // boolval() 函数用于把值转为布尔值
             $favored = boolval($user->favoriteProducts()->find($product->id));
         }
-
+        
         $reviews = OrderItem::query()
-            ->with(['order.user', 'productSku']) // 预先加载关联关系
+            ->with(['order.user', 'productSku'])// 预先加载关联关系
             ->where('product_id', $product->id)
-            ->whereNotNull('reviewed_at') // 筛选出已评价的
-            ->orderBy('reviewed_at', 'desc') // 按评价时间倒序
-            ->limit(10) // 取出 10 条
+            ->whereNotNull('reviewed_at')// 筛选出已评价的
+            ->orderBy('reviewed_at', 'desc')// 按评价时间倒序
+            ->limit(10)// 取出 10 条
             ->get();
-
+        
         // 最后别忘了注入到模板中
         return view('products.show', [
             'product' => $product,
@@ -81,31 +99,31 @@ class ProductsController extends Controller
             'reviews' => $reviews
         ]);
     }
-
+    
     public function favor(Product $product, Request $request)
     {
         $user = $request->user();
         if ($user->favoriteProducts()->find($product->id)) {
             return [];
         }
-
+        
         $user->favoriteProducts()->attach($product);
-
+        
         return [];
     }
-
+    
     public function disfavor(Product $product, Request $request)
     {
         $user = $request->user();
         $user->favoriteProducts()->detach($product);
-
+        
         return [];
     }
-
+    
     public function favorites(Request $request)
     {
         $products = $request->user()->favoriteProducts()->paginate(16);
-
+        
         return view('products.favorites', ['products' => $products]);
     }
 }
