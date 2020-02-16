eda42e62c2891d5a3c4d7366e4aeb8c853bd6109
jeff <waagik@gmail.com>
2020-02-16 13:38:49 +0800

    1、初始化分期付款模型 2、引入 moontoast/math 库来进行精度计算 3、封闭返回BigNumber实例的助手函数

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -16,12 +16,34 @@ function ngrok_url($routeName, $parameters = [])
     return route($routeName, $parameters);
 }
 
+/**
+ * 返回BigNumber实例，默认的精度为小数点后两位
+ * @param $number
+ * @param int $scale
+ * @return \Moontoast\Math\BigNumber
+ */
+function big_number($number, $scale = 2)
+{
+    return new \Moontoast\Math\BigNumber($number, $scale);
+}
+
+/**
+ * 记录任意级别日志
+ * @param $level
+ * @param $msg
+ * @param array $context
+ */
 function _log($level, $msg, $context = [])
 {
     \Log::write($level, $msg, $context);
 }
 
 
+/**
+ * 记录debug级别日志
+ * @param $msg
+ * @param $context
+ */
 function logDebug($msg, $context)
 {
     _log('debug', $msg, $context);
