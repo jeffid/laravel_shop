ba0d69ee50fe295bf7913e3134bb150bfa72b380
jeff <waagik@gmail.com>
2020-02-20 17:48:33 +0800

    1、商品属性模型 2、商品属性表直接关系到商品表，一个属性一条记录，一个商品可以有多个同名属性，同名属性合并展示 3、groupBy()集合方法可以以参数名作为键，来返回结果集

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
     public static $typeMap = [
-        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_NORMAL => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
     ];
     
@@ -22,12 +22,7 @@ class Product extends Model
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
-    // 与商品SKU关联
-    public function skus()
-    {
-        return $this->hasMany(ProductSku::class);
-    }
-
+    
     public function getImageUrlAttribute()
     {
         // 如果 image 字段本身就已经是完整的 url 就直接返回
@@ -35,6 +30,23 @@ class Product extends Model
             return $this->attributes['image'];
         }
         return \Storage::disk('public')->url($this->attributes['image']);
+    }
+    
+    public function getGroupedPropertiesAttribute()
+    {
+        return $this->properties
+            // 按照属性名聚合，返回的集合的 key 是属性名，value 是包含该属性名的所有属性集合
+            ->groupBy('name')
+            ->map(function ($properties) {
+                // 使用 map 方法将属性集合变为属性值集合
+                return $properties->pluck('value')->all();
+            });
+    }
+    
+    // 与商品SKU关联
+    public function skus()
+    {
+        return $this->hasMany(ProductSku::class);
     }
     
     public function category()
@@ -45,5 +57,10 @@ class Product extends Model
     public function crowdfunding()
     {
         return $this->hasOne(CrowdfundingProduct::class);
+    }
+    
+    public function properties()
+    {
+        return $this->hasMany(ProductProperty::class);
     }
 }
